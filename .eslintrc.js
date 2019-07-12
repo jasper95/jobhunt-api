f16c3943b2cf9bb3c6950d67309a16865309fab6
Jasper Bernales <jasper@dnamicro.com>
2019-07-12 09:14:47 +0800

    fix eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,26 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "rules": {
-        "semi": 0,
-        "comma-dangle": ["error", "never"],
-        "class-methods-use-this": 0,
-        "camelcase": 0,
-        "no-param-reassign": 0,
-        "import/prefer-default-export": 0,
-        "no-throw-literal": 0
-    },
-    "globals": {
-        "Promise": true,
-        "util": true
-    },
-    "settings": {
-        "import/resolver": {
-          "node": {
-            "moduleDirectory": [
-              "./node_modules",
-              "./src"
-            ]
-          }
-        }
+  "extends": "airbnb-base",
+  "rules": {
+      "semi": 0,
+      "comma-dangle": ["error", "never"],
+      "class-methods-use-this": 0,
+      "camelcase": 0,
+      "no-param-reassign": 0,
+      "import/prefer-default-export": 0,
+      "no-throw-literal": 0
+  },
+  "globals": {
+      "Promise": true,
+      "util": true
+  },
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "moduleDirectory": [
+          "./node_modules",
+          "./src"
+        ]
       }
-}
+    }
+  }
 };
